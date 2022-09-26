8ad59137fd4c38feddceb9f21b38a0bc47a8245f
Keshav Grover <keshavgrover84@yahoo.com>
2022-09-26 19:16:26 +0530

    Add Income-Expenses component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
       <Header />
+      <Balance />
+      <IncomeExpenses />
     </div>
   );
 }
